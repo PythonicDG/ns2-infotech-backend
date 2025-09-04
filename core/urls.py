a88922eb37397df619adffb1901bbf1f02c352c4
PythonicDG <dipakgaikwadmg@gmail.com>
2025-09-04 17:21:29 +0530

    [add] contact api

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HeaderFooterAPIView
+from .views import *
 
 urlpatterns = [
     path('header-footer/', HeaderFooterAPIView.as_view(), name='header-footer'),
+    path('contact/', contact_form_submit, name='contact-form-submit'),
 ]
--- core/views.py
+++ core/views.py
@@ -14,6 +14,10 @@ from .serializers import (
     CompanyProfileSerializer,
 )
 
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import ContactMessage
+
 
 class HeaderFooterAPIView(APIView):
     def get(self, request, format=None):
@@ -42,3 +46,36 @@ class HeaderFooterAPIView(APIView):
         }
 
         return Response(data)
+
+@api_view(['POST'])
+def contact_form_submit(request):
+    data = request.data
+
+    full_name = data.get('full_name')
+    email = data.get('email_address')
+    phone = data.get('phone_number')
+    subject = data.get('subject')
+    message = data.get('message')
+
+    if not full_name or not email or not subject or not message:
+        return Response({
+            "error": "Full name, email, subject, and message are required."
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        contact = ContactMessage.objects.create(
+            full_name=full_name,
+            email_address=email,
+            phone_number=phone,
+            subject=subject,
+            message=message
+        )
+        return Response({
+            "message": "success"
+        }, status=status.HTTP_201_CREATED)
+
+    except Exception as e:
+        return Response({
+            "error": "Something went wrong.",
+            "details": str(e)
+        }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
