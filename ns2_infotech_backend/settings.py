264f0ca97ecae6cf78ce0720f9956d12323034c4
PythonicDG <dipakgaikwadmg@gmail.com>
2025-09-09 13:41:49 +0530

    [CHANGE]

--- ns2_infotech_backend/settings.py
+++ ns2_infotech_backend/settings.py
@@ -6,7 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-replace-this')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = ['*']
 
 TEMPLATES = [
     {
