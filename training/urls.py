e995d8df3ef7813a0080c565e4b11c3aa9173199
PythonicDG <dipakgaikwadmg@gmail.com>
2025-09-13 02:02:41 +0530

    [fix] downoad api issue

--- training/urls.py
+++ training/urls.py
@@ -3,5 +3,5 @@ from .views import PageSectionTraining, download_brochure
 
 urlpatterns = [
     path('fetch-training-page/', PageSectionTraining.as_view(), name='page-section-list'),
-    path('download_brochure/<int:pk>/', download_brochure, name='download-brochure'),
+    path('download_brochure/<int:pk>', download_brochure, name='download-brochure'),
 ]
