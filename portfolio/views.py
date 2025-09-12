3f4a8597dec3e38c53deffa85a53555fd790f00b
PythonicDG <dipakgaikwadmg@gmail.com>
2025-09-13 02:30:09 +0530

    [remove] changes

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,13 +9,12 @@ logger = logging.getLogger(__name__)
 
 class PortfolioSectionsAPIView(APIView):
     def get(self, request, format=None):
-        data = cache.get('portfolio_sections')
-        if not data:
-            sections = PageSection.objects.filter(is_active=True) \
-                .prefetch_related('content_items') \
-                .order_by('order')
-            serializer = PageSectionSerializer(sections, many=True)
-            data = serializer.data
-            cache.set('portfolio_sections', data, 60 * 5)
+        sections = PageSection.objects.filter(is_active=True) \
+            .prefetch_related('content_items') \
+            .order_by('order')
 
-        return Response(data)
+        serializer = PageSectionSerializer(sections, many=True)
+
+        response = Response(serializer.data)
+
+        return response
