177bc62c738c48132eb131d71139ac3a0b67e026
PythonicDG <dipakgaikwadmg@gmail.com>
2025-09-13 02:29:02 +0530

    [add] cache in portfolio api

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,12 +9,13 @@ logger = logging.getLogger(__name__)
 
 class PortfolioSectionsAPIView(APIView):
     def get(self, request, format=None):
-        sections = PageSection.objects.filter(is_active=True) \
-            .prefetch_related('content_items') \
-            .order_by('order')
+        data = cache.get('portfolio_sections')
+        if not data:
+            sections = PageSection.objects.filter(is_active=True) \
+                .prefetch_related('content_items') \
+                .order_by('order')
+            serializer = PageSectionSerializer(sections, many=True)
+            data = serializer.data
+            cache.set('portfolio_sections', data, 60 * 5)
 
-        serializer = PageSectionSerializer(sections, many=True)
-
-        response = Response(serializer.data)
-
-        return response
+        return Response(data)
