8ff90c410f494f5089db7e945f9435cd830c4ee2
PythonicDG <dipakgaikwadmg@gmail.com>
2025-09-15 18:31:14 +0530

    [revert]

--- training/views.py
+++ training/views.py
@@ -22,17 +22,6 @@ class PageSectionTraining(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-# @api_view(['GET'])
-# def download_brochure(request, pk):
-#     content_item = get_object_or_404(SectionContent, pk=pk)
-#     brochure_file = content_item.brochures
-
-#     if not brochure_file:
-#         return Response({"detail": "Brochure file not found."}, status=status.HTTP_404_NOT_FOUND)
-
-#     file_url = request.build_absolute_uri(brochure_file.url)
-#     return Response({"brochure_url": file_url}, status=status.HTTP_200_OK)
-
 @api_view(['GET'])
 def download_brochure(request, pk):
     content_item = get_object_or_404(SectionContent, pk=pk)
@@ -41,12 +30,5 @@ def download_brochure(request, pk):
     if not brochure_file:
         return Response({"detail": "Brochure file not found."}, status=status.HTTP_404_NOT_FOUND)
 
-    file_path = brochure_file.path
-    if not os.path.exists(file_path):
-        return Response({"detail": "File does not exist."}, status=status.HTTP_404_NOT_FOUND)
-
-    response = FileResponse(open(file_path, 'rb'), as_attachment=True, filename=os.path.basename(file_path))
-    response['Access-Control-Allow-Origin'] = 'https://nsinfotech.networkindia.com'
-    response['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-    response['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization'
-    return response
+    file_url = request.build_absolute_uri(brochure_file.url)
+    return Response({"brochure_url": file_url}, status=status.HTTP_200_OK)
