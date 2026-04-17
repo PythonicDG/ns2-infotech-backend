fd9a1f31468541cfae5e58b66276bfe59d7d1e15
PythonicDG <dipakgaikwadmg@gmail.com>
2026-04-18 01:42:56 +0530

    feat: initialize project URL routing with API endpoints and media/static serving configurations

--- ns2_infotech_backend/urls.py
+++ ns2_infotech_backend/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', lambda request: redirect('admin/')),
@@ -20,5 +20,7 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
